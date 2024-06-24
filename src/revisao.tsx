73fc7b93ec1be94aac097d7b69c3e63d9566541f
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-06-24 09:27:48 -0400

    Update revisao.tsx

--- src/revisao.tsx
+++ src/revisao.tsx
@@ -1,6 +1,23 @@
-function Revisao(){
-    return(
-        <h1>Componente</h1>
+type P ={
+    nome: string;
+    idade: number;
+    cpf: string;
+}
+function Revisao():JSX.Element{
+    const a:number = 10
+    const b: number[] = [10,20,30]
+    const pessoa:P  = {
+        nome:"Tere",
+        idade:29,
+        cpf:"123.123.123-12"
+    }
+    return( 
+        <>
+            <h1>Componente</h1>
+            {pessoa.nome}
+            {pessoa.idade}
+            {pessoa.cpf}
+        </>
     )
 }
 export default Revisao;
