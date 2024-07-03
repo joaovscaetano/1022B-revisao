6244571aef3322ee9752fbc35ab6325be0ab0d42
João Caetano <118355857+joaovscaetano@users.noreply.github.com>
2024-07-03 08:44:43 -0400

    Update revisao.tsx

--- src/revisao.tsx
+++ src/revisao.tsx
@@ -3,9 +3,11 @@ import { ChangeEvent, useState } from "react";
 function Revisao():JSX.Element{
     const [a,setA] = useState(0)
     const [b,setB] = useState(0)
+    
     function soma(a:number,b:number):number{
         return a+b
     }
+    
     function trataInput1(e:ChangeEvent<HTMLInputElement>){
         setA(Number(e.target.value))
     }
@@ -19,20 +21,38 @@ function Revisao():JSX.Element{
     //EX: listaNumeros(10,15) 
     //retorno "10,11,12,13,14,15,"
 
+    
     function listaNumeros(a1:number,b1:number):string{
         let lista:string = ""
         //Fazer um processamento dos números
         //let str = "10,"
-        let i = a1
-        while(i<=b1){
+        let maior =a1>b1 ? a1:b1
+        let menor = a1<=b1 ? a1:b1
+        let i = maior
+        while(i >= menor){
             lista = lista.concat(String(i)+",")
-            i++
+            i--
         }
         return lista
     }
+    
+    
+    /*
+    function somaNumeros(a1:number, b1:number):String{
+        let lista:String = ""
 
+        let maior = 0
+        let menor = 0
 
-    return( 
+        let i = menor
+        while(i >= maior){
+            lista = lista.concat(String(i)+",")
+            i = 1 + menor
+        }
+    }                                                                                                                                                                                   
+    */
+
+    return(
         <>
             <h1>Componente</h1>
             <input type="number" onChange={trataInput1}/>
